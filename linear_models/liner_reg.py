986c1f1581e1a60ef773e7095a7d647078bd4003
dzodrzo888 <jirkaapesek@seznam.cz>
2025-07-03 14:46:49 +0200

    Added logistic regression + comments and accuracy to linear

--- linear_models/liner_reg.py
+++ linear_models/liner_reg.py
@@ -1,85 +1,157 @@
+"""This module is used to create LinearRegression class."""
 import numpy as np
-import matplotlib.pyplot as plt
 
-def generate_data(n_samples=100, noise_std=0.8):
-    np.random.seed(42)
-    X = 2 * np.random.rand(n_samples, 1)
-    true_w = 3.7
-    true_b = 2.5
-    y = true_w * X + true_b + np.random.randn(n_samples, 1) * noise_std
-    return X, y
-
-def predict(X, w, b):
+class LinearRegression:
     """
-    Predicts y_hat using the lin reg equation
-
-    Args:
-        X (np.array): X
-        w (float): weights
-        b (float): bias
-
-    Returns:
-        np.array: Predicted y value
+    Class to calculate, fit and meassure accuracy of a lin_reg model.
     """
-    y_hat = w * X + b
-    return y_hat
 
-def compute_cost(y_hat, y):
-    m = len(y)
-    mse = np.sum(np.square(y_hat - y)) / m
-    return mse
+    def __init__(self, learning_rate=0.01, epochs=1000):
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.weights = None
+        self.bias = None
+        self.losses = []
 
-def compute_gradients(X, y, y_hat):
-    dw = (2/len(y))*np.sum((y_hat - y)*X)
-    db = (2/len(y))*np.sum(y_hat - y)
-    return dw, db
+    def _initialize_parameters(self, n_features):
+        """
+        Initialize parametrs (weights, bias)
 
-def train(X, y, learning_rate=0.01, epochs=1000):
-    w = 0.0
-    b = 0.0
+        Args:
+            n_features (int): Number of features
+        """
+        # Weights initialization
+        self.weights = np.zeros(n_features)
+        # Bias initialization
+        self.bias = 0.0
 
-    losses = []
-    
-    for epoch in range(epochs):
+    def predict(self, X):
+        """
+        Predicts y_hat using the lin reg equation
 
-        y_hat = predict(X, w, b)
+        Args:
+            X (np.array): X
+            w (float): weights
+            b (float): bias
 
-        cost = compute_cost(y_hat, y)
+        Returns:
+            np.array: Predicted y value
+        """
+        # Compute predictions
+        y_hat = np.dot(X, self.weights) + self.bias
+        return y_hat
 
-        losses.append(cost)
+    def _compute_cost(self, y, y_hat):
+        """
+        Compute cost function
 
-        dw, db = compute_gradients(X, y, y_hat)
+        Args:
+            y (np.array): Y actual values.
+            y_hat (np.array): Y predicted.
 
-        w -= learning_rate * dw
-        b -= learning_rate * db
+        Returns:
+            mse(float): Mean squared error.
+        """
+        m = len(y)
+        # MSE calculation
+        mse = np.sum(np.square(y_hat - y)) / m
+        return mse
 
-        if epoch % 100 == 0:
-            print(f"Epoch {epoch:4}: Cost = {cost:.4f} | w = {w:.4f}, b = {b:.4f}")
+    def _compute_gradients(self, X, y, y_hat):
+        """
+        Computes gradients.
 
-    return w, b, losses
+        Args:
+            X (np.array): X feature.
+            y (np.array): Y actual values.
+            y_hat (np.array): Y predicted.
 
-def plot_results(X, y, w, b, losses):
-    # Plot regression line
-    plt.subplot(1, 2, 1)
-    plt.scatter(X, y, color='blue', label='Data')
-    y_pred = predict(X, w, b)
-    plt.plot(X, y_pred, color='red', label='Regression Line')
-    plt.xlabel("X")
-    plt.ylabel("y")
-    plt.legend()
-    plt.title("Fitted Line")
+        Returns:
+            dw(np.array): Derivative with respect to w.
+            db(float): Derivative with respect to b.
+        """
+        # Calculate derivate values
+        dw = (2/len(y))*np.dot(X.T, (y_hat - y))
+        db = (2/len(y))*np.sum(y_hat - y)
 
-    # Plot loss over time
-    plt.subplot(1, 2, 2)
-    plt.plot(losses, label='Loss')
-    plt.xlabel("Epoch")
-    plt.ylabel("MSE Loss")
-    plt.title("Training Loss")
-    plt.legend()
-    plt.tight_layout()
-    plt.show()
+        return dw, db
 
-if __name__ == "__main__":
-    X, y = generate_data()
-    w, b, losses = train(X, y, learning_rate=0.05, epochs=1000)
-    plot_results(X, y, w, b, losses)
+    def fit(self, X, y):
+        """
+        Trains the linear regression model on inputed data.
+
+        Args:
+            X (np.array): X feature.
+            y (np.array): Y actual value.
+        """
+        _, n_features = X.shape
+        # Initialize parametrs
+        self._initialize_parameters(n_features)
+
+        for epoch in range(self.epochs):
+            # linear model
+            y_hat = self.predict(X)
+
+            cost = self._compute_cost(y, y_hat)
+
+            self.losses.append(cost)
+
+            dw, db = self._compute_gradients(X, y, y_hat)
+
+            # Calcilate gradient descent
+            self.weights -= self.learning_rate * dw
+            self.bias -= self.learning_rate * db
+
+            if epoch % 100 == 0:
+                print(f"Epoch {epoch:4}: Cost = {cost:.4f}")
+
+    def rmse_calc(self, y, y_hat):
+        """
+        Computes squared mse.
+
+        Args:
+            y (np.array): Y actual values.
+            y_hat (np.array): Y predicted.
+
+        Returns:
+            float: Root mean squared error.
+        """
+        return np.sqrt(self._compute_cost(y, y_hat))
+
+    def r_sqrt_calc(self, y, y_hat):
+        """
+        Calcualtes R squared
+
+        Args:
+            y (np.array): Y actual values.
+            y_hat (np.array): Y predicted.
+
+        Returns:
+            float: R squared.
+        """
+
+        # Calculate mean
+        y_mean = np.mean(y)
+
+        # Calcualte sum of squares
+        ss_res = np.sum(np.square(y-y_hat))
+        ss_total = np.sum(np.square(y-y_mean))
+
+        # Calculate r squared
+        r_sqrt = 1 - (ss_res/ss_total)
+
+        return r_sqrt
+
+if __name__ == '__main__':
+    from sklearn.datasets import make_regression
+    from sklearn.model_selection import train_test_split
+
+    X, y = make_regression(n_samples=1000, n_features=10, noise=10, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    acc = model.r_sqrt_calc(y_test, y_pred)
+    print(f"Test accuracy: {acc:.4f}")
---
+++ linear_models/log_reg.py
@@ -0,0 +1,189 @@
+"""This module is used to create LogisticRegression class."""
+import numpy as np
+
+class LogisticRegression:
+    """
+    Class to calculate, fit and meassure accuracy of a log_reg model.
+    """
+    def __init__(self, learning_rate=0.01, epochs=1000):
+        # Initialize variables
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+        self.weights = None
+        self.bias = None
+        self.losses = []
+
+    def _sigmoid(self, z):
+        """
+        Sigmoid activation function.
+
+        Args:
+            z (np.array): Logit.
+
+        Returns:
+            sigmoid (np.array): Sigmoid.
+        """
+        # Sigmoid function calculation
+        sigmoid = 1/(1+np.exp(-z))
+
+        return sigmoid
+
+    def _initialize_parameters(self, n_features):
+        """
+        Initialize parametrs (weights, bias)
+
+        Args:
+            n_features (int): Number of features
+        """
+        # Weights initialization
+        self.weights = np.zeros(n_features)
+        # Bias initialization
+        self.bias = 0.0
+
+    def _compute_predictions(self, X):
+        """
+        Computes logit predictions.
+
+        Args:
+            X (np.array): X feature.
+
+        Returns:
+            z(np.array): Logits.
+        """
+        # Compute predictions
+        z=np.dot(X, self.weights) + self.bias
+
+        return z
+
+    def _compute_gradients(self, X, y, y_pred):
+        """
+        Computes gradients.
+
+        Args:
+            X (np.array): X feature.
+            y (np.array): Y actual values.
+            y_pred (np.array): Y predicted.
+
+        Returns:
+            dw(np.array): Derivative with respect to w.
+            db(float): Derivative with respect to b.
+        """
+        # Initialize variables
+        m = len(y)
+        error = y_pred - y
+
+        # Calculate derivatives values
+        dw = (1/m)*np.dot(X.T, error)
+        db = (1/m)*np.sum(error)
+
+        return dw, db
+
+    def _loss(self, y, y_pred):
+        """
+        Compute loss.
+
+        Args:
+            y (np.array): Y actual values.
+            y_pred (np.array): Y predicted.
+
+        Returns:
+            cost (float): Cost.
+        """
+        # Set vars - Add epsilon to prevent -inf
+        epsilon = 1e-15
+        y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+
+        # Calculate loss function
+        loss = -y*np.log(y_pred)-(1-y)*np.log(1-y_pred)
+        cost = np.mean(loss)
+
+        return cost
+
+    def fit(self, X, y):
+        """
+        Trains the logistic regression model on inputed data.
+
+        Args:
+            X (np.array): X feature.
+            y (np.array): Y actual value.
+        """
+        # Set vars
+        _, n_features = X.shape
+        self._initialize_parameters(n_features)
+
+        for i in range(self.epochs):
+            # Linear model
+            lin_pred = self._compute_predictions(X)
+            y_hat = self._sigmoid(lin_pred)
+
+            dw, db = self._compute_gradients(X, y, y_hat)
+
+            # Calculate gradient descent
+            self.weights -= self.learning_rate*dw
+            self.bias -= self.learning_rate*db
+
+            # Print losses
+            if i % 100 == 0:
+                loss = self._loss(y, y_hat)
+                self.losses.append(loss)
+                print(f"Iteration {i}, Loss: {loss:.4f}")
+
+    def predict_proba(self, X):
+        """
+        Predict propabilities
+
+        Args:
+            X (np.array): X features
+
+        Returns:
+            y_hat (np.array): Predictions based on the model.
+        """
+        # Get predictions
+        y_hat = self._sigmoid(self._compute_predictions(X))
+
+        return y_hat
+
+    def predict(self, X):
+        """
+        Predict cls labels based on the X input
+
+        Args:
+            X (np.array): X features.
+
+        Returns:
+            cls_labels (np.array): Class labels
+        """
+        # Predict cls labels.
+        cls_labels = (self.predict_proba(X) >= 0.5).astype(int)
+        
+        return cls_labels
+
+    def accuracy(self, y_true, y_pred):
+        """
+        Calculates accuracy of predictions.
+
+        Args:
+            y_true (np.array): True y values.
+            y_pred (np.array): Predicted y values
+
+        Returns:
+            float: The accuracy of predictions
+        """
+        # Get accuracy of predictions
+        return np.mean(y_true == y_pred)
+
+# Example usage
+if __name__ == "__main__":
+    # Generate dummy dataset
+    from sklearn.datasets import make_classification
+    from sklearn.model_selection import train_test_split
+
+    X, y = make_classification(n_samples=1000, n_features=10, n_classes=2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    model = LogisticRegression(learning_rate=0.1, epochs=1000)
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    acc = model.accuracy(y_test, y_pred)
+    print(f"Test accuracy: {acc:.4f}")
