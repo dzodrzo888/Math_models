a2e759ba8f2195ad4c3c874216ca38f84e754f77
dzodrzo888 <jirkaapesek@seznam.cz>
2025-07-15 17:03:18 +0200

    Added dense and sequential functions

--- NN/nn_bin_cls.py
+++ NN/nn_bin_cls.py
@@ -7,22 +7,6 @@ class BinaryNN:
     def __init__(self):
         ...
 
-    def compute_predictions(self, X: np.array, w: float, b: float) -> np.array:
-        """
-        Calculates predictions.
-
-        Args:
-            x (np.array): Input values.
-            w (float): Weights.
-            b (float): Bias.
-
-        Returns:
-            z(np.array): Prediction
-        """
-        z = np.dot(X, w) + b
-
-        return z
-
     def sigmoid_calc(self, z: np.array) -> np.array:
         """
         Function to calculate sigmoid value
@@ -37,5 +21,48 @@ class BinaryNN:
 
         return a
 
-    def dense(self):
+    def dense(self, A_in: np.array, W: np.array, b: np.array) -> np.array:
+        """
+        NN layer used to 
+
+        Args:
+            A_in (np.array): Input vector
+            W (np.array): Weights vector
+            b (np.array): Bias vector
+
+        Returns:
+            A_out(np.array): _description_
+        """
+        A_out_logit = np.matmul(A_in, W) + b
+
+        A_out = self.sigmoid_calc(A_out_logit)
+
+        return A_out
+
+    def sequential(self, X: np.array, weights: list[np.array], biases: np.array) -> np.array:
+        """
+        Sequential func used to calculate the propability
+
+        Args:
+            X (np.array): Input vector
+            weights (list[np.array]): List of weights
+            biases (np.array): Vector of biases.
+
+        Returns:
+            a_out(np.array): Prob value of a_out
+        """
+        units = weights.shape[0]
+
+        for j in range(units):
+
+            if j == 0:
+                a_j = self.dense(X, weights[0], biases[0])
+            else:
+                a_j = self.dense(a_j, weights[j], biases[j])
         
+        a_out = a_j
+
+        return a_out
+        
+if __name__ == "__main__":
+    ...
