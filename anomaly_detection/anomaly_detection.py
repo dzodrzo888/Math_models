aa65ae42793df3860c795dd4e51970bb1d195e9f
dzodrzo888 <jirkaapesek@seznam.cz>
2025-07-22 10:07:41 +0200

    Added print feature to select_threshold

--- anomaly_detection/anomaly_detection.py
+++ anomaly_detection/anomaly_detection.py
@@ -1,3 +1,4 @@
+"""This module is used to get gaussian anomaly detection"""
 import numpy as np
 
 class GaussianAnomalyDetection:
@@ -6,7 +7,7 @@ class GaussianAnomalyDetection:
         self.epsilon = epsilon
         self.mu = None
         self.var = None
-    
+
     def _estimate_gaussian(self, X: np.array):
         """
         Estimates mean and variance
@@ -23,7 +24,7 @@ class GaussianAnomalyDetection:
         var = np.var(X, axis=0)
 
         return mu, var
-    
+
     def _multivariate_gaussian(self, X: np.array, mu: np.array, var: np.array) -> np.array:
         """
         Computes propabilites using gaussian
@@ -36,13 +37,13 @@ class GaussianAnomalyDetection:
         Returns:
             np.array: Propabilites
         """
-        
+
         exponent = -0.5 * np.square(X - mu) / var
         scaling = 1 / np.sqrt(2 * np.pi * var)
         probs = np.prod(scaling * np.exp(exponent), axis=1)
 
         return probs
-    
+
     def select_threshold(self, y_val: np.array, prob_val: np.array) -> float:
         """
         Selects the most optimal epsilon value.
@@ -51,16 +52,14 @@ class GaussianAnomalyDetection:
             y_val (np.array): True anomalies
             prob_val (np.array): Propabilities of a anomaly
         """
-        
+
         best_epsilon = 0
         best_f1 = 0
 
-        min_prob, max_prob = min(prob_val), max(prob_val)
-
-        epsilons = np.logspace(np.log10(min_prob), np.log10(max_prob), num=1000)
+        epsilons = np.logspace(np.log10(min(prob_val)), np.log10(max(prob_val)), num=1000)
 
         for epsilon in epsilons:
-            
+
             predictions = prob_val < epsilon
             tp = np.sum(np.logical_and(predictions, y_val))
             fp = np.sum(np.logical_and(predictions, np.logical_not(y_val)))
@@ -75,8 +74,9 @@ class GaussianAnomalyDetection:
             if f1 > best_f1:
                 best_f1 = f1
                 best_epsilon = epsilon
-        
+
         self.epsilon = best_epsilon
+        print(f"Threshold was selected with the best epsilon: {self.epsilon} and the best f1: {best_f1}.")
 
     def fit(self, X: np.array):
         """
@@ -85,7 +85,7 @@ class GaussianAnomalyDetection:
         Args:
             X (np.array): Input features
         """
-        
+
         self.mu, self.var = self._estimate_gaussian(X=X)
 
     def get_propabilities(self, X:np.array) -> np.array:
@@ -104,7 +104,7 @@ class GaussianAnomalyDetection:
 
         if self.mu is None or self.var is None:
             raise ValueError("Fit the model before calculating propabilities!")
-        
+
         return self._multivariate_gaussian(X=X, mu=self.mu, var=self.var)
 
     def predict(self, X: np.array):
@@ -120,10 +120,10 @@ class GaussianAnomalyDetection:
         Returns:
             list: List of values.
         """
-        
+
         if self.epsilon is None:
             raise ValueError("Epsilon is None! Run select_threshold or set epsilon!")
-        
+
         probs = self._multivariate_gaussian(X=X, mu=self.mu, var=self.var)
 
         return (probs < self.epsilon).astype(int)
